01536a151a4f857157edf9a2510e9dfa5ff00d46
a0937027831 <a0937027831@users.noreply.github.com>
2024-07-31 16:05:00 +0000

    deploy: 6c4a90341fd8bf61507fce4229fff8d42df3da4e

--- src/define/define-global-component.js
+++
@@ -1,9 +0,0 @@
-import XInput from '@/components/form/input.vue'
-import XTable from '@/components/table/table.vue'
-import XDialog from '@/components/modal/dialog.vue'
-
-export function registerGlobalComponents(app) {
-  app.component('XInput', XInput);
-  app.component('XTable', XTable);
-  app.component('XDialog', XDialog);
-}
--- src/stores/loading.js
+++
@@ -1,9 +0,0 @@
-import { ref } from 'vue'
-import { defineStore } from 'pinia'
-
-export const useLoadingStore = defineStore('loading', () => {
-  const isLoading = ref(false);
-  const setLoading = (value) =>{ isLoading.value = value; }
-  
-  return { isLoading, setLoading }
-})
