6e8585e96c8a7d0cda47f0bfac70303aeed2aa50
DESKTOP-QMEC56B\CHOU <a0937027831@gmail.com>
2024-07-31 18:24:00 +0800

    add vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <BaseLayut>
+    <RouterView />
+  </BaseLayut>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import BaseLayut from '@/layout/base-layout'
+</script>
+
+
+<style></style>
---
+++ src/layout/base-layout.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <NavBar></NavBar>
+    <v-main>
+      <div class="page">
+        <slot></slot>
+      </div>
+    </v-main>
+    <Loading></Loading>
+  </v-app>
+
+</template>
+
+<script setup>
+import NavBar from '@/components/navbar/navbar'
+import Loading from '@/components/modal/loading'
+</script>
+
+<style lang="scss">
+.page {
+  display: flex;
+  flex-direction: column;
+  padding: 8px 16px;
+}
+</style>
