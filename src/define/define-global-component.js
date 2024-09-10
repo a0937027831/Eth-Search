96269bb12b8804a9ee68efbc856f4b6477889c5a
TonyCHOU <tonychou@chainsecurity.asia>
2024-09-10 16:36:31 +0800

    add search table

--- src/define/define-global-component.js
+++ src/define/define-global-component.js
@@ -1,9 +1,11 @@
 import XInput from '@/components/form/input.vue'
 import XTable from '@/components/table/table.vue'
+import XSearchTable from '@/components/table/searchTable.vue'
 import XDialog from '@/components/modal/dialog.vue'
 
 export function registerGlobalComponents(app) {
   app.component('XInput', XInput);
   app.component('XTable', XTable);
+  app.component('XSearchTable',XSearchTable)
   app.component('XDialog', XDialog);
 }
